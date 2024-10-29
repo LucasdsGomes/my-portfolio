f897dde0de3a7e0082c819fdc88893ca9776b9e0
Lucas Gomes <programmingpyrubygo@gmail.com>
2024-10-28 23:06:54 -0300

    dir components created

---
+++ src/components/Banner.tsx
@@ -0,0 +1,68 @@
+import { Col, Container, Row } from "react-bootstrap";
+import { ArrowRightCircle } from "react-bootstrap-icons";
+import headerImg from "../assets/img/header-img.svg";
+import { useState, useEffect } from "react";
+
+export const Banner = () => {
+    const [loopNum, setLoopNum] = useState(0);
+    const [isDeleting, setIsDeleting] = useState(false);
+    const toRotate = ["Backend Dev", "Automation Engine", "FullStack Dev"]
+    const [text, setText] = useState('');
+    const [delta, setDelta] = useState(300 - Math.random() * 100);
+    const period = 2000;
+
+    useEffect(() => {
+        const timer = setInterval(() => {
+            tick();
+        }, delta)
+
+        return () => { clearInterval(timer)};
+    }, [text]);
+
+    const tick = () => {
+        let i = loopNum % toRotate.length;
+        let fullText = toRotate[i];
+        let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1)
+        setText(updatedText);
+
+        if (isDeleting) {
+            setDelta((prevDelta: number) => prevDelta / 2)
+        }
+
+        if (!isDeleting && updatedText === fullText) {
+            setIsDeleting(true);
+            setDelta(period);
+        } else if (isDeleting && updatedText === '') {
+            setIsDeleting(false);
+            setLoopNum(loopNum + 1);
+            setDelta(500);
+        }
+    }
+
+    return (
+        <section className="banner" id="home">
+            <Container>
+                <Row className="align-items-center">
+                    <Col xs={12} md={6} xl={7}>
+                        <span className="tagline">Welcome to my Portfolio</span>
+                        <h1>{`Hi! I'm Lucas Gomes`} <span className="wrap">{text}</span> </h1>
+                        <p>Hello, my name is Lucas Gomes, it's a pleasure to meet you! 
+
+                        I'm a curious and dedicated Back-end developer committed to continuous learning. I have a proactive spirit that seeks effective solutions and new operational perspectives for the team. With experience in Python, Golang, Java, HTML, CSS, JavaScript, among other languages, I deliver promising projects in the job market.
+
+                        If you're looking for a programmer who can bring technical skills along with a wide range of soft skills and a strong commitment to productivity and innovation, you've just found one.
+
+                        Let's connect!
+
+                        Currently pursuing a degree in Technology in Systems Analysis and Development in the Sixth Semester - Evening Period
+                        </p>
+                        <button onClick={() => console.log('connect')}>Let's Connect! <ArrowRightCircle size={25} /></button>
+                    </Col>
+                    <Col xs={12} md={6} xl={5}>
+                        <img src={headerImg} alt="Header Img" />
+                    </Col>
+                </Row>
+            </Container>
+        </section>
+    )
+}
