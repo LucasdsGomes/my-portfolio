830ee77bb6140a8b6029622659e6586fc24afa15
Lucas Gomes <programmingpyrubygo@gmail.com>
2024-10-30 12:03:04 -0300

    added footer at app

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Banner } from './components/Banner';
 import { Skills } from './components/Skills';
 import { Projects } from './components/Projects';
 import { Contact } from './components/Contact';
+import { Footer } from './components/Footer';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
 
@@ -16,6 +17,7 @@ function App() {
         <Skills />
         <Projects />
         <Contact />
+        <Footer />
       </div>
     </>
   )
