859ffecf59d6b1e719598b5b7bd68bf9da27e8ee
Lucas Gomes <programmingpyrubygo@gmail.com>
2024-10-30 13:30:23 -0300

    changing devLogo

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { Nav, Container, Navbar } from "react-bootstrap";
-import logo from "../assets/img/logo.svg";
+import logo from "../assets/img/devLogo.png";
 import navIcon1 from "../assets/img/nav-icon1.svg";
 import navIcon2 from "../assets/img/nav-icon2.svg";
 import navIcon3 from "../assets/img/nav-icon3.svg";
@@ -34,7 +34,7 @@ export const NavBar = () => {
       <Navbar expand="lg" className={scrolled ? "scrolled" : ""}>
         <Container>
           <Navbar.Brand href="#home">
-            <img src={logo} alt="Logo" />
+            <img src={logo} id="logo" alt="Logo" />
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <span className="navbar-toggle-icon"></span>
