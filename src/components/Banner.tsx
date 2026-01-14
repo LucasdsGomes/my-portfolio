feb9760cd767cfd58bf33c52167852e304151d0a
Lucas de Souza Gomes <programmingpyrubygo@gmail.com>
2026-01-14 00:32:01 -0300

    feat(core): adding and updating languages with new technologies that i learn, projects cards are now clickable and updating IG

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -2,69 +2,87 @@ import { Col, Container, Row } from "react-bootstrap";
 import { ArrowRightCircle } from "react-bootstrap-icons";
 import headerImg from "../assets/img/header-img.svg";
 import { useState, useEffect } from "react";
-import 'animate.css';
-import TrackVisibility from 'react-on-screen';
-import { useTranslation } from 'react-i18next';
+import "animate.css";
+import TrackVisibility from "react-on-screen";
+import { useTranslation } from "react-i18next";
+
+const toRotate = ["Backend Dev", "Data Analyst", "Fullstack"];
 
 export const Banner = () => {
-    const { t } = useTranslation();
-    const [loopNum, setLoopNum] = useState(0);
-    const [isDeleting, setIsDeleting] = useState(false);
-    const toRotate = ["Backend Dev", "Bot Dev", "Scrapping Dev"];
-    const [text, setText] = useState('');
-    const [delta, setDelta] = useState(300 - Math.random() * 100);
-    const period = 2000;
+  const { t } = useTranslation();
+  const [loopNum, setLoopNum] = useState(0);
+  const [isDeleting, setIsDeleting] = useState(false);
+  const [text, setText] = useState("");
+  const [delta, setDelta] = useState(300 - Math.random() * 100);
+  const period = 2000;
 
-    useEffect(() => {
-        const timer = setInterval(() => {
-            tick();
-        }, delta);
-
-        return () => clearInterval(timer);
-    }, [text, delta, isDeleting]);
-
+  useEffect(() => {
     const tick = () => {
-        const i = loopNum % toRotate.length;
-        const fullText = toRotate[i];
-        const updatedText = isDeleting
-            ? fullText.substring(0, text.length - 1)
-            : fullText.substring(0, text.length + 1);
-        setText(updatedText);
+      const i = loopNum % toRotate.length;
+      const fullText = toRotate[i];
+      const updatedText = isDeleting
+        ? fullText.substring(0, text.length - 1)
+        : fullText.substring(0, text.length + 1);
+      setText(updatedText);
 
-        if (isDeleting) {
-            setDelta(prevDelta => prevDelta / 2);
-        }
+      if (isDeleting) {
+        setDelta((prevDelta) => prevDelta / 2);
+      }
 
-        if (!isDeleting && updatedText === fullText) {
-            setIsDeleting(true);
-            setDelta(period);
-        } else if (isDeleting && updatedText === '') {
-            setIsDeleting(false);
-            setLoopNum(prevLoopNum => prevLoopNum + 1);
-            setDelta(500);
-        }
+      if (!isDeleting && updatedText === fullText) {
+        setIsDeleting(true);
+        setDelta(period);
+      } else if (isDeleting && updatedText === "") {
+        setIsDeleting(false);
+        setLoopNum((prevLoopNum) => prevLoopNum + 1);
+        setDelta(500);
+      }
     };
 
-    return (
-        <section className="banner" id="home">
-            <Container>
-                <Row className="align-items-center">
-                    <Col xs={12} md={6} xl={7}>
-                        <TrackVisibility>
-                        {({ isVisible }) => 
-                            <div className={isVisible ? "animated__animated animate__bounce" : ""}>
-                                <span className="tagline">{t("banner.tagline")}</span>
-                                <h1>{t("banner.greeting")} <span className="wrap">{text}</span></h1>
-                                <p>{t("banner.description")}</p>
-                                <button onClick={() => console.log('connect')}>{t("banner.button")} <ArrowRightCircle size={25} /></button>
-                            </div>}
-                        </TrackVisibility>
-                    </Col>
-                    <Col xs={12} md={6} xl={5}>
-                        <img src={headerImg} alt="Header Img" />
-                    </Col>
-                </Row>
-            </Container>
-        </section>
-    );
+    const timer = setInterval(() => {
+      tick();
+    }, delta);
+
+    return () => clearInterval(timer);
+  }, [text, delta, isDeleting, loopNum]);
+
+  return (
+    <section className="banner" id="home">
+      <Container>
+        <Row className="align-items-center">
+          <Col xs={12} md={6} xl={7}>
+            <TrackVisibility>
+              {({ isVisible }) => (
+                <div
+                  className={
+                    isVisible ? "animated__animated animate__bounce" : ""
+                  }
+                >
+                  <span className="tagline">{t("banner.tagline")}</span>
+                  <h1>
+                    {t("banner.greeting")} <span className="wrap">{text}</span>
+                  </h1>
+                  <p>{t("banner.description")}</p>
+                  <button
+                    onClick={() =>
+                      window.open(
+                        "https://www.linkedin.com/in/lucasdsgomes/",
+                        "_blank",
+                        "noopener,noreferrer"
+                      )
+                    }
+                  >
+                    {t("banner.button")} <ArrowRightCircle size={25} />
+                  </button>
+                </div>
+              )}
+            </TrackVisibility>
+          </Col>
+          <Col xs={12} md={6} xl={5}>
+            <img src={headerImg} alt="Header Img" />
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
 };
--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,24 +1,34 @@
-import { Col } from "react-bootstrap"
+import { Col } from "react-bootstrap";
 
 interface Components {
-    title: string;
-    description: string;
-    image: string;
+  title: string;
+  description: string;
+  image: string;
+  github: string;
 }
 
-// react functional components
-// export const ProjectCard: React.FC = ({ title: string, description: string, image: string}) => {}
-
-export const ProjectCard = ({ title, description, image }: Components) => {
-    return (
-        <Col sm={6} md={4}>
-            <div className="proj-imgbx">
-                <img src={image} />
-                <div className="proj-txtx">
-                    <h4>{title}</h4>
-                    <span>{description}</span>
-                </div>
-            </div>
-        </Col>
-    )
-}
+export const ProjectCard = ({
+  title,
+  description,
+  image,
+  github,
+}: Components) => {
+  return (
+    <Col sm={6} md={4}>
+      <a
+        href={github}
+        target="_blank"
+        rel="noopener noreferrer"
+        style={{ textDecoration: "none", color: "inherit" }}
+      >
+        <div className="proj-imgbx">
+          <img src={image} />
+          <div className="proj-txtx">
+            <h4>{title}</h4>
+            <span>{description}</span>
+          </div>
+        </div>
+      </a>
+    </Col>
+  );
+};
